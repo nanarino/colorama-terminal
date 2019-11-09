552e2d5610bdbe99495ebd43c4a2badfc37dea3b
nanarino <49119482+nanarino@users.noreply.github.com>
2019-11-09 22:17:56 +0800

    CREATE  main

---
+++ colorama-terminal.py
@@ -0,0 +1,71 @@
+#Compatible module of terminal color for Win7 and more
+from colorama import init, Fore
+init(autoreset=True)
+
+#Connot set attributes of built-in/extension type
+def _print(*args,r = False):
+    args = list(args)
+    for i, arg in enumerate(args[:]):
+        if isinstance(arg,bool):
+            args[i] = Fore.GREEN + arg.__str__() + Fore.RESET
+        elif isinstance(arg,str):
+            args[i] = Fore.YELLOW +"\'"+ arg.__str__() +"\'"+ Fore.RESET
+        elif isinstance(arg,int) or isinstance(arg,float) or isinstance(arg,complex):
+            args[i] = Fore.CYAN + arg.__str__() +Fore.RESET
+        #print 'list' 'tuple' 'set' or 'dict' directly will lose color by calling '__repr__'
+        elif isinstance(arg,tuple):
+            args[i] = '('
+            for j in arg:
+                args[i] += _print(j,r = True) + ', '
+            args[i] = (args[i][:-1] or '(') + ')'
+        elif isinstance(arg,list):
+            args[i] = '['
+            for j in arg:
+                args[i] += _print(j,r = True) + ', '
+            args[i] = (args[i][:-2] or '[') + ']'
+        elif isinstance(arg,set):
+            args[i] = ''
+            for j in arg:
+                args[i] += _print(j,r = True) + ', '
+            args[i] = ['set()','{' + args[i][:-2] + '}'][bool(args[i])]
+        elif isinstance(arg,dict):
+            args[i] = '{'
+            for j,k in arg.items():
+                args[i] += _print(j,r = True) + ': ' + _print(k,r = True) + ', '
+            args[i] = (args[i][:-2] or '{') + '}'
+        elif args[i] is None:
+            args[i] = Fore.MAGENTA + 'None' + Fore.RESET
+        elif isinstance(arg,BaseException):
+            args[i] = Fore.RED + arg.__repr__() + Fore.RESET
+        else:
+            pass
+    if r:
+        return args[0]
+    else:
+        print(*args)
+
+
+if __name__ == "__main__":
+    while 1:
+        ex = input('>>> ')
+        try:
+            try:
+                _print(eval(ex))
+            except SyntaxError as e:
+                exec(ex)
+        except Exception as e:
+            print("Traceback (most recent call last):")
+            _print(e)
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
