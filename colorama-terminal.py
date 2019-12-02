92eae6ac010df289b3368e87776320e81cc62396
nanarino <49119482+nanarino@users.noreply.github.com>
2019-12-02 15:45:59 +0800

    MOD        Decimal按num处理

--- colorama-terminal.py
+++ colorama-terminal.py
@@ -1,41 +1,41 @@
 #Compatible module of terminal color for Win7 and more
 from colorama import init, Fore
-init(autoreset=True)
+init(autoreset = True)
 
 #Connot set attributes of built-in/extension type
-def _print(*args,r = False):
+def _print(*args, r = False):
     args = list(args)
     for i, arg in enumerate(args[:]):
-        if isinstance(arg,bool):
+        if isinstance(arg, bool):
             args[i] = Fore.GREEN + arg.__str__() + Fore.RESET
-        elif isinstance(arg,str):
-            args[i] = Fore.YELLOW +"\'"+ arg.__str__() +"\'"+ Fore.RESET
-        elif isinstance(arg,int) or isinstance(arg,float) or isinstance(arg,complex):
-            args[i] = Fore.CYAN + arg.__str__() +Fore.RESET
+        elif isinstance(arg, str):
+            args[i] = Fore.YELLOW + "\'" + arg.__str__() + "\'" + Fore.RESET
+        elif isinstance(arg, (int, float, complex)) or str(type(arg)) == "<class 'decimal.Decimal'>":
+            args[i] = Fore.CYAN + arg.__str__() + Fore.RESET
         #print 'list' 'tuple' 'set' or 'dict' directly will lose color by calling '__repr__'
-        elif isinstance(arg,tuple):
+        elif isinstance(arg, tuple):
             args[i] = '('
             for j in arg:
                 args[i] += _print(j,r = True) + ', '
             args[i] = (args[i][:-1] or '(') + ')'
-        elif isinstance(arg,list):
+        elif isinstance(arg, list):
             args[i] = '['
             for j in arg:
                 args[i] += _print(j,r = True) + ', '
             args[i] = (args[i][:-2] or '[') + ']'
-        elif isinstance(arg,set):
+        elif isinstance(arg, set):
             args[i] = ''
             for j in arg:
                 args[i] += _print(j,r = True) + ', '
             args[i] = ['set()','{' + args[i][:-2] + '}'][bool(args[i])]
-        elif isinstance(arg,dict):
+        elif isinstance(arg, dict):
             args[i] = '{'
             for j,k in arg.items():
                 args[i] += _print(j,r = True) + ': ' + _print(k,r = True) + ', '
             args[i] = (args[i][:-2] or '{') + '}'
         elif args[i] is None:
             args[i] = Fore.MAGENTA + 'None' + Fore.RESET
-        elif isinstance(arg,BaseException):
+        elif isinstance(arg, BaseException):
             args[i] = Fore.RED + arg.__repr__() + Fore.RESET
         else:
             args[i] = arg.__str__()
